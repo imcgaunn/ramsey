72d5f2b183bc3ae9ba974079584367fa1e0888f8
Ian McGaunn <ian.mcgaunn@bluecoat.com>
2016-03-04 11:30:33 -0500

    Added some initial budgeting rules functions

---
+++ src/budget/rules.clj
@@ -0,0 +1,22 @@
+(ns budget.rules)
+
+(def weeks-per-month 4)
+
+(defn monthly-income 
+  "computes pay per month based on a pay amount and an interval in weeks"
+  [pay interval]
+  (* pay (/ weeks-per-month interval)))
+
+(defn valid-rules? 
+  "Ensures that the ruleset allocates a maximum of 100% of income" 
+  [rules]
+  (<= 100 (reduce + (vals rules))))
+
+(defn resources-per-category 
+  "Computes allocation of resources per category based on rule"
+  [income rules]
+  (let [amnts 
+        (map (fn [rule] (* income (/ (second rule) 100))) 
+             rules)]
+    (zipmap (keys rules) amnts)))
+ 
