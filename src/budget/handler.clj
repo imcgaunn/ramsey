d79f665c5247bf0f43a55814c3f4663af1315abd
Ian McGaunn <ian.mcgaunn@bluecoat.com>
2016-03-07 11:30:41 -0500

    Added default scaffolding and disabled ring security for testing

--- src/budget/handler.clj
+++ src/budget/handler.clj
@@ -1,11 +1,13 @@
 (ns budget.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.util.response :as resp]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (resp/redirect "/index.html"))
+  (GET "/compute-budget" [] "Reponse text")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes (assoc site-defaults :security false)))
