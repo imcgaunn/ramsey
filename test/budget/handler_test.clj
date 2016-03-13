7c49dbd68acade715a5455e189905a8ad04f9dcc
Ian McGaunn <ian.mcgaunn@bluecoat.com>
2016-03-13 07:29:39 -0400

    Added unworking test for /compute-budget backend endpoint

--- test/budget/handler_test.clj
+++ test/budget/handler_test.clj
@@ -1,14 +1,23 @@
 (ns budget.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
+            [clojure.data.json :as json]
+            [budget.rules :as rules]
             [budget.handler :refer :all]))
 
-(deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+(def valid-budget-map {:cat1-title "Title 1"
+                       :cat1-val "30"
+                       :cat2-title "Title 2"
+                       :cat2-val "70"})
 
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+(def invalid-budget-map {:cat1-title "Title 1"
+                       :cat1-val "30"
+                       :cat2-title "Title 2"
+                       :cat2-val "80"})
+
+; TODO: fix this test 
+(deftest test-app
+  (testing "compute-budget route"
+    (let [response (app (mock/request :post "/compute-budget" 
+                                      valid-budget-map))]
+      (println response))))
