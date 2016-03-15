52543c5310d0b0cf8f7c4bad396711362f372229
Ian McGaunn <ian.mcgaunn@bluecoat.com>
2016-03-15 13:49:39 -0400

    Specified budget_date column as primary key and updated insert query

--- src/queries/create_budget_db.sql
+++ src/queries/create_budget_db.sql
@@ -1,6 +1,6 @@
 -- Creates budget database table if not already created
 CREATE TABLE IF NOT EXISTS budgets (
-  budget_date text,
+  budget_date text PRIMARY KEY,
   categories text,
   income real
 );
