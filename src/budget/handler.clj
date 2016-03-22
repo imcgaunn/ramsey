9be2c46d0bf3ce67edb4fbb592ebc8e9a6980171
Ian McGaunn <ian.mcgaunn@bluecoat.com>
2016-03-22 12:45:24 -0400

    Updated compute-budget endpoint to properly insert to database
    Correspondingly, added GET endpoint to retrieve budget information for
    a given year and month.
    Exception handling code also added for compute-budget endpoint handler.

--- src/budget/handler.clj
+++ src/budget/handler.clj
@@ -4,16 +4,30 @@
             [clojure.pprint :refer [pprint]]
             [clojure.data.json :as json]
             [budget.rules :as rules]
+            [budget.database :as db]
             [ring.util.response :as resp]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] (resp/redirect "/index.html"))
+  (GET "/" []
+       (resp/content-type
+         (resp/resource-response "index.html" {:root "public"})
+         "text/html"))
+  (GET "/month/:m/year/:y" [m y] (db/monthly-budget m y))
   (POST "/compute-budget" req 
         (try
-          (json/write-str (rules/form-data->computed-budget (:params req)))
+          (let [params (:params req)
+                budget (json/write-str (rules/form-data->computed-budget params))
+                date   (str (:month params) "-" (:year params))
+                income (:income params)]
+            (db/insert-month-budget! {:mdate date
+                                      :budget budget
+                                      :income income})
+            {:status 200 :body "successfully inserted budget data"})
           (catch AssertionError e
-            {:status 400 :body "invalid budget rules"})))
+            {:status 400 :body "invalid budget rules"})
+          (catch Exception e
+            {:status 400 :body "couldn't insert budget"})))
   (route/not-found "Not Found"))
 
 (def app
